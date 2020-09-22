d5c12e1fe0dd6f814773972b96d77154776d858b
MalteZea <70883688+MalteZea@users.noreply.github.com>
2020-09-22 11:51:08 +0200

    ændring

--- src/A_3_25_GamePickACard/Main.java
+++ src/A_3_25_GamePickACard/Main.java
@@ -1,8 +1,41 @@
 package A_3_25_GamePickACard;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /* (Game: pick a card) Write a program that simulates picking a card from a deck
 of 52 cards. Your program should display the rank (Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 Jack, Queen, King) and suit (Clubs, Diamonds, Hearts, Spades) of the card. */
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        /*  Bruger input er kun symbolsk, og bruges til at få brugeren til at
+            føle, at vedkommende har en påvirkning på progammet.
+            Brugerens input bruges slet ikke efterfølgende. */
+
+        System.out.print("Pick a card between 1 and 52: ");
+        int card = input.nextInt();
+
+        switch (Carddeck.Choice()) {
+            case 1:
+                System.out.print("You picked Ace of ");
+                break;
+            case 11:
+                System.out.print("You picked Jack of ");
+                break;
+            case 12:
+                System.out.print("You picked Queen of ");
+                break;
+            case 13:
+                System.out.print("You picked King of ");
+                break;
+            default:
+                System.out.print("You picked " + Carddeck.Choice() + " of ");
+        }
+        System.out.println(Carddeck.SuitChoice());
+
+    }
 }
+
--- src/A_3_35_EvenOrOdd/Main.java
+++ src/A_3_35_EvenOrOdd/Main.java
@@ -1,7 +1,22 @@
 package A_3_35_EvenOrOdd;
+import java.util.Scanner;
 
 /* (Even or odd number) Write a program that prompts the user to enter an integer and
 displays whether the integer is an odd number or not. */
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please input a whole number: ");
+        int number = input.nextInt();
+
+        int oddOrEven = number % 2;
+
+        if (oddOrEven == 1) {
+            System.out.println(number + " is an odd number");
+        } else System.out.println(number + " is an even number");
+
+
+    }
 }
